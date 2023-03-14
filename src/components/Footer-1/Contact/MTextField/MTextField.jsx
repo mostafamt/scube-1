b4379047ba49306d5f69b6f60439009a802e5ec4
mostafamt <mostafamt60@yahoo.com>
2023-03-14 18:11:49 +0200

    update form error colors

---
+++ src/components/Footer-1/Contact/MTextField/MTextField.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./mTextField.module.scss";
+
+const MTextField = () => {
+  return <input type="text" className={styles.box} />;
+};
+
+export default MTextField;
